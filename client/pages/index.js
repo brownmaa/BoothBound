a01726d0632550819adbabc1b0388617698f8f01
brownmaa <51038637+brownmaa@users.noreply.github.com>
2025-04-17 18:31:33 +0000

    Add minimal Home page so Next.js can build

---
+++ client/pages/index.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main style={{display:"flex",justifyContent:"center",alignItems:"center",height:"100vh",fontFamily:"sans-serif"}}>
+      <h1>BoothBound is live 🚀</h1>
+    </main>
+  );
+}
