3929c88e465a2e2aa637f2014ade1d6180407b8c
brownmaa <51038637+brownmaa@users.noreply.github.com>
2025-04-18 03:52:24 +0000

    Fix NavBar import to relative path

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,5 @@
 import "../styles/globals.css";
-import NavBar from "@/components/NavBar";
+import NavBar from "../components/NavBar";
 
 export default function App({ Component, pageProps }) {
   return (
